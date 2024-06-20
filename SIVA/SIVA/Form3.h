9c317d98d152e2ac1e7563ad8f922eebb2b08027
Julio <jaxt3k@gmail.com>
2024-06-20 10:53:13 -0600

    2.3

--- SIVA/SIVA/Form3.h
+++ SIVA/SIVA/Form3.h
@@ -35,17 +35,40 @@ namespace SIVA {
 			}
 		}
 	private: System::Windows::Forms::Label^ label1;
+	protected:
 	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::Button^ sugh;
-	private: System::Windows::Forms::Button^ plgh;
-	private: System::Windows::Forms::Button^ pagh;
-	private: System::Windows::Forms::Button^ shgh;
+
+
+
+
 	private: System::Windows::Forms::Label^ label3;
 	private: System::Windows::Forms::Label^ label4;
 	private: System::Windows::Forms::Label^ label5;
 	private: System::Windows::Forms::Label^ label6;
-	private: System::Windows::Forms::Button^ comprar;
-	private: System::Windows::Forms::Button^ devolver;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -66,16 +89,10 @@ namespace SIVA {
 		{
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->sugh = (gcnew System::Windows::Forms::Button());
-			this->plgh = (gcnew System::Windows::Forms::Button());
-			this->pagh = (gcnew System::Windows::Forms::Button());
-			this->shgh = (gcnew System::Windows::Forms::Button());
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->label5 = (gcnew System::Windows::Forms::Label());
 			this->label6 = (gcnew System::Windows::Forms::Label());
-			this->comprar = (gcnew System::Windows::Forms::Button());
-			this->devolver = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// label1
@@ -101,65 +118,12 @@ namespace SIVA {
 			this->label2->Text = L"GymShark";
 			this->label2->Click += gcnew System::EventHandler(this, &Form3::label2_Click);
 			// 
-			// sugh
-			// 
-			this->sugh->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
-			this->sugh->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->sugh->ForeColor = System::Drawing::SystemColors::ControlLightLight;
-			this->sugh->Location = System::Drawing::Point(160, 313);
-			this->sugh->Name = L"sugh";
-			this->sugh->Size = System::Drawing::Size(133, 51);
-			this->sugh->TabIndex = 3;
-			this->sugh->Text = L"AGREGAR";
-			this->sugh->UseVisualStyleBackColor = false;
-			this->sugh->Click += gcnew System::EventHandler(this, &Form3::vsugh_Click);
-			// 
-			// plgh
-			// 
-			this->plgh->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
-			this->plgh->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->plgh->ForeColor = System::Drawing::SystemColors::ControlLightLight;
-			this->plgh->Location = System::Drawing::Point(299, 313);
-			this->plgh->Name = L"plgh";
-			this->plgh->Size = System::Drawing::Size(133, 51);
-			this->plgh->TabIndex = 4;
-			this->plgh->Text = L"AGREGAR";
-			this->plgh->UseVisualStyleBackColor = false;
-			// 
-			// pagh
-			// 
-			this->pagh->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
-			this->pagh->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->pagh->ForeColor = System::Drawing::SystemColors::ControlLightLight;
-			this->pagh->Location = System::Drawing::Point(438, 313);
-			this->pagh->Name = L"pagh";
-			this->pagh->Size = System::Drawing::Size(133, 51);
-			this->pagh->TabIndex = 5;
-			this->pagh->Text = L"AGREGAR";
-			this->pagh->UseVisualStyleBackColor = false;
-			// 
-			// shgh
-			// 
-			this->shgh->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
-			this->shgh->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->shgh->ForeColor = System::Drawing::SystemColors::ControlLightLight;
-			this->shgh->Location = System::Drawing::Point(577, 313);
-			this->shgh->Name = L"shgh";
-			this->shgh->Size = System::Drawing::Size(133, 51);
-			this->shgh->TabIndex = 6;
-			this->shgh->Text = L"AGREGAR";
-			this->shgh->UseVisualStyleBackColor = false;
-			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Verdana", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(165, 145);
+			this->label3->Location = System::Drawing::Point(176, 362);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(112, 46);
 			this->label3->TabIndex = 7;
@@ -172,7 +136,7 @@ namespace SIVA {
 			this->label4->AutoSize = true;
 			this->label4->Font = (gcnew System::Drawing::Font(L"Verdana", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label4->Location = System::Drawing::Point(324, 145);
+			this->label4->Location = System::Drawing::Point(335, 362);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(91, 46);
 			this->label4->TabIndex = 8;
@@ -185,7 +149,7 @@ namespace SIVA {
 			this->label5->AutoSize = true;
 			this->label5->Font = (gcnew System::Drawing::Font(L"Verdana", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label5->Location = System::Drawing::Point(472, 145);
+			this->label5->Location = System::Drawing::Point(483, 362);
 			this->label5->Name = L"label5";
 			this->label5->Size = System::Drawing::Size(80, 46);
 			this->label5->TabIndex = 9;
@@ -198,38 +162,12 @@ namespace SIVA {
 			this->label6->AutoSize = true;
 			this->label6->Font = (gcnew System::Drawing::Font(L"Verdana", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label6->Location = System::Drawing::Point(603, 145);
+			this->label6->Location = System::Drawing::Point(614, 362);
 			this->label6->Name = L"label6";
 			this->label6->Size = System::Drawing::Size(80, 46);
 			this->label6->TabIndex = 10;
 			this->label6->Text = L"Short\r\n$1500";
 			this->label6->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
-			// 
-			// comprar
-			// 
-			this->comprar->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
-			this->comprar->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->comprar->ForeColor = System::Drawing::SystemColors::ControlLightLight;
-			this->comprar->Location = System::Drawing::Point(207, 396);
-			this->comprar->Name = L"comprar";
-			this->comprar->Size = System::Drawing::Size(208, 47);
-			this->comprar->TabIndex = 11;
-			this->comprar->Text = L"COMPRAR";
-			this->comprar->UseVisualStyleBackColor = false;
-			// 
-			// devolver
-			// 
-			this->devolver->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
-			this->devolver->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->devolver->ForeColor = System::Drawing::SystemColors::ControlLightLight;
-			this->devolver->Location = System::Drawing::Point(452, 396);
-			this->devolver->Name = L"devolver";
-			this->devolver->Size = System::Drawing::Size(208, 47);
-			this->devolver->TabIndex = 12;
-			this->devolver->Text = L"DEVOLVER";
-			this->devolver->UseVisualStyleBackColor = false;
 			// 
 			// Form3
 			// 
@@ -237,16 +175,10 @@ namespace SIVA {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ActiveBorder;
 			this->ClientSize = System::Drawing::Size(884, 461);
-			this->Controls->Add(this->devolver);
-			this->Controls->Add(this->comprar);
 			this->Controls->Add(this->label6);
 			this->Controls->Add(this->label5);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->label3);
-			this->Controls->Add(this->shgh);
-			this->Controls->Add(this->pagh);
-			this->Controls->Add(this->plgh);
-			this->Controls->Add(this->sugh);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
 			this->Name = L"Form3";
