cb771af8a133cd58243fa5d8218ef2e290c0ccf7
Huerta Sánchez Diego Salvador <168474776+diegoo178@users.noreply.github.com>
2024-06-21 10:53:44 -0600

    Create Structejemplo.cpp

---
+++ Structejemplo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+struct Persona {
+    string nombre;
+    int edad;
+    float salario;
+};
+
+int main() {
+    Persona p1; 
+
+    cout << "Ingrese nombre: ";
+    cin >> p1.nombre;
+    cout << "Ingrese edad: ";
+    cin >> p1.edad;
+    cout << "Ingrese salario: ";
+    cin >> p1.salario;
+
+    cout << "\nDatos de la persona:" << endl;
+    cout << "Nombre: " << p1.nombre << endl;
+    cout << "Edad: " << p1.edad << endl;
+    cout << "Salario: " << p1.salario << endl;
+
+    return 0;
+}
